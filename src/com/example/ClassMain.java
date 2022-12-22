5e9aaf07f08837c182da3052e8c9c4f8bd58756b
ana cossio <anacossio7@gmail.com>
2022-12-22 17:47:06 -0300

    first delivery

---
+++ src/com/example/ClassMain.java
@@ -0,0 +1,33 @@
+package com.example;
+
+public class ClassMain {
+    public static void main(String[] args){
+        byte numByte=1;
+        short numShort=2;
+        int numInt=3;
+        long numLong=1234678799;
+        // punto flotante
+        float numFloat=4.9f;
+        double numDouble=2.99d;
+
+        //caracter
+        char varChar='H';
+        //boolean
+        boolean varBool1=true;
+        boolean varBool2=false;
+
+        String varString="Hello World";
+        System.out.println("num byte: "+ numByte);
+        System.out.println("num short: "+ numShort);
+        System.out.println("int "+numInt);
+        System.out.println("long "+numLong);
+        System.out.println("float "+numFloat);
+        System.out.println("double "+numDouble);
+        System.out.println("boolean "+varBool1);
+        System.out.println("boolean "+varBool2);
+        System.out.println("char "+varChar);
+        System.out.println("string "+varString);
+
+
+    }
+}
